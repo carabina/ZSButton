c92cdd1d9ba2913d57c72d6f57d5440a81c6c03d
shize <shize@youmi.net>
2017-12-06 15:19:26 +0800

    test

---
+++ ZSButton/Classes/ColorButton.h
@@ -0,0 +1,12 @@
+//
+//  ColorButton.h
+//  ZSButton
+//
+//  Created by swinpa on 2017/12/6.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorButton : UIButton
+
+@end
---
+++ ZSButton/Classes/ColorButton.m
@@ -0,0 +1,20 @@
+//
+//  ColorButton.m
+//  ZSButton
+//
+//  Created by swinpa on 2017/12/6.
+//
+
+#import "ColorButton.h"
+
+@implementation ColorButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
